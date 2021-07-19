bb389a6059b0a981f6dbe8a81314f6065bd080ea
A01423328 <a01423328@itesm.mx>
2021-07-19 15:16:49 -0500

    shows the images that were on the firestore

--- src/comps/ImageGrid.js
+++ src/comps/ImageGrid.js
@@ -2,10 +2,16 @@ import React from 'react'
 import useFirestore from '../hooks/useFirestore'
 
 const ImageGrid = () => {
-    const { docs } = useFirestore('images');
-    console.log(docs);
+    const { docs } = useFirestore('images'); // gets the firestore database data and saves it on the 'docs' variable
+    // console.log(docs);
     return (
-        <div className="img-grid">images</div>
+        <div className="img-grid">
+            { docs && docs.map( doc => ( // cycles through the docs data
+                <div className="img-wrap" key={doc.id}>
+                    <img src={doc.url} alt="uploaded picture" />
+                </div>
+            ))}
+        </div>
     )
 }
 
