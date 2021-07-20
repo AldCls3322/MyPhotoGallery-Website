1efca04e745c1b96d661302df481d9c9d34c30d4
A01423328 <a01423328@itesm.mx>
2021-07-20 15:40:58 -0500

    added animations using framer-motion

--- src/comps/ImageGrid.js
+++ src/comps/ImageGrid.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import useFirestore from '../hooks/useFirestore'
+import useFirestore from '../hooks/useFirestore';
+import { motion } from 'framer-motion';
 
 const ImageGrid = ( { setSelectedImage } ) => {
     const { docs } = useFirestore('images'); // gets the firestore database data and saves it on the 'docs' variable
@@ -7,9 +8,13 @@ const ImageGrid = ( { setSelectedImage } ) => {
     return (
         <div className="img-grid">
             { docs && docs.map( doc => ( // cycles through the docs data
-                <div className="img-wrap" key={doc.id} onClick={() => setSelectedImage(doc.url)}>
-                    <img src={doc.url} alt="uploaded picture" />
-                </div>
+                <motion.div className="img-wrap" key={doc.id} /* using framer-motion */
+                    layout // animates to the new positions, when an image is uploaded
+                    whileHover={{ opacity: 1 }} // by framer-motion package
+                    onClick={() => setSelectedImage(doc.url)} 
+                >
+                    <motion.img src={doc.url} alt="uploaded picture" initial={{ opacity: 0 }} animate={{ opacity:1 }} transition={{ delay: 1 }} />
+                </motion.div>
             ))}
         </div>
     )
--- src/comps/ProgressBar.js
+++ src/comps/ProgressBar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useEffect } from 'react';
 import useStorage from '../hooks/useStorage';
+import { motion } from 'framer-motion';
 
 const ProgressBar = ( {file, setFile}) => {
     const { url, progress } = useStorage(file);
@@ -12,7 +13,7 @@ const ProgressBar = ( {file, setFile}) => {
     }, [url, setFile]) // we pass url and setFile drom this function
 
     return (
-        <div className="progress-bar" style={{ width: progress + "%" }}></div>
+        <motion.div className="progress-bar" initial={{ width: 0 }} animate={{ width: progress + "%" }}></motion.div>
     )
 }
 
